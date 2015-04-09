faf5cef88346b5c865eec9bb66fd00f5fd619925
Ankita <ankita.agrawal1113@gmail.com>
2015-04-09 19:11:03 +0530

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,23 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require jquery-migrate-1.2.1.min
+//= require bootstrap.min
+//= require jquery.ui.touch-punch.min
+//= require jquery.autosize.min
+//= require jquery.placeholder.min
+//= require jquery-jvectormap-1.2.2.min
+//= require jquery-jvectormap-world-mill-en
+//= require custom.min
+//= require core.min
---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,16 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def submenu1
+  end
+
+  def submenu2
+  end
+
+  def submenu3
+  end
+
+  def submenu4
+  end
+end
